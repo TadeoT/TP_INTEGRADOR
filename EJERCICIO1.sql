c10c2772b1b917735be53acf0dbccb4a18a25a29
Tiraboschi Tadeo <tadetiraboschi@gmail.com>
2020-11-24 12:16:06 -0300

    EJERCICIO 1 TERMINADO

--- EJERCICIO1.sql
+++ EJERCICIO1.sql
@@ -48,7 +48,7 @@ CREATE table importacion.localidad
 categoria varchar(120) not null,
 centroide_lat decimal not null,
 centroide_lon decimal not null,
-departamento_id varchar(30) not null,
+departamento_id varchar(30),
 departamento_nombre varchar(120) not null,
 fuente varchar(30) not null,
 funcion varchar(30) null,
@@ -60,28 +60,13 @@ provincia_id integer not null,
 provincia_nombre varchar (120) not null
 )
 
+UPDATE importacion.localidad SET departamento_id = NULL  where departamento_id = ''
+
+
 copy importacion.localidad from 'C:\Users\tadeo\Dropbox\Facultad\Base_de_Datos\TP INTEGRADOR 2\Data\localidades-censales.csv' with delimiter ',' CSV HEADER;
 
 
------------------------
--- MUNICIPIOS
------------------------
 
-CREATE table importacion.municipio
-(
-categoria varchar(120) not null,
-centroide_lat decimal not null,
-centroide_lon decimal not null,
-fuente varchar(120) not null,
-id integer not null,
-nombre 	varchar (120) not null,
-nombre_completo varchar(120) not null,
-provincia_id integer not null,
-provincia_interseccion decimal not null,
-provincia_nombre varchar (120) not null
-)
-
-copy importacion.municipio from 'C:\Users\tadeo\Dropbox\Facultad\Base_de_Datos\TP INTEGRADOR 2\Data\municipios.csv' with delimiter ',' CSV HEADER;
 
 
 -------------------- PASAMOS AL SCHEMA TERRITORIO-------------------------------------
@@ -93,6 +78,6 @@ INSERT INTO territorio.provincia (id_provincia,identificador,nombre)
 INSERT INTO territorio.departamento (id_departamento,id_provincia,identificador,nombre) 
         SELECT d.id, d.provincia_id, d.id, d.nombre_completo FROM importacion.departamento d
 
---- PROBLEMA AL CARGAR ID_DEPARTAMENTO----
-INSERT INTO territorio.municipio (id_municipio,id_departamento,identificador,nombre) 
-        SELECT m.id, m.provincia_id, m.id, m.nombre_completo FROM importacion.municipio m
+
+INSERT INTO territorio.localidad (id_localidad,id_provincia,id_departamento,identificador,nombre) 
+        SELECT l.id, l.provincia_id, CAST(l.departamento_id AS integer), l.id, l.nombre FROM importacion.localidad l
--- ModificacionEstructura.sql
+++ ModificacionEstructura.sql
@@ -3,8 +3,9 @@ ALTER TABLE territorio.provincia ALTER COLUMN nombre TYPE character varying (120
 ALTER TABLE territorio.departamento ALTER COLUMN id_departamento TYPE integer
 ALTER TABLE territorio.departamento ALTER COLUMN identificador TYPE integer 
 
-
-ALTER TABLE territorio.municipio ALTER COLUMN id_municipio TYPE integer
-ALTER TABLE territorio.municipio ALTER COLUMN identificador TYPE integer 
-
-
+ALTER TABLE territorio.localidad ALTER COLUMN id_departamento TYPE integer 
+ALTER TABLE territorio.localidad ALTER COLUMN id_departamento DROP NOT NULL;
+ALTER TABLE territorio.localidad ALTER COLUMN id_provincia TYPE integer 
+ALTER TABLE territorio.localidad ALTER COLUMN id TYPE integer 
+ALTER TABLE territorio.localidad ALTER COLUMN identificador TYPE integer 
+ALTER TABLE territorio.localidad ALTER COLUMN nombre TYPE character varying (120) 
