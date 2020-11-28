725f46a3065fad64315f06396202b0e06f85eeab
Tiraboschi Tadeo <tadetiraboschi@gmail.com>
2020-11-28 19:40:03 -0300

    terminado ejercicio 5

--- EJERCICIO5.sql
+++ EJERCICIO5.sql
@@ -7,12 +7,12 @@ EXECUTE PROCEDURE tr_persona_fisica_insert();
 
 CREATE TRIGGER trPersona_fisica_U
 AFTER
-INSERT ON personas.personafisica FOR EACH ROW
+UPDATE ON personas.personafisica FOR EACH ROW
 EXECUTE PROCEDURE tr_persona_fisica_update();
   
 CREATE TRIGGER trPersona_fisica_D
 AFTER
-INSERT ON personas.personafisica FOR EACH ROW
+DELETE ON personas.personafisica FOR EACH ROW
 EXECUTE PROCEDURE tr_persona_fisica_delete();
 
 --- PERSONA JURIDICA TRIGGER----------------
@@ -23,12 +23,12 @@ EXECUTE PROCEDURE tr_persona_juridica_insert();
 
 CREATE TRIGGER trPersona_juridica_U
 AFTER
-INSERT ON personas.personajuridica FOR EACH ROW
+UPDATE ON personas.personajuridica FOR EACH ROW
 EXECUTE PROCEDURE tr_persona_juridica_update();
 
 CREATE TRIGGER trPersona_juridica_D
 AFTER
-INSERT ON personas.personajuridica FOR EACH ROW
+DELETE ON personas.personajuridica FOR EACH ROW
 EXECUTE PROCEDURE tr_persona_juridica_delete();
 
 --- ORGANISMO ENTIDAD TRIGGER----------------
@@ -39,12 +39,12 @@ EXECUTE PROCEDURE tr_organismo_entidad_insert();
 
 CREATE TRIGGER trOrganismo_entidad_U
 AFTER
-INSERT ON personas.organismoentidad FOR EACH ROW
+UPDATE ON personas.organismoentidad FOR EACH ROW
 EXECUTE PROCEDURE tr_organismo_entidad_update();
 
 CREATE TRIGGER trOrganismo_entidad_D
 AFTER
-INSERT ON personas.organismoentidad FOR EACH ROW
+DELETE ON personas.organismoentidad FOR EACH ROW
 EXECUTE PROCEDURE tr_organismo_entidad_delete();
 
 CREATE TABLE auditoria.log_persona_fisica(
@@ -55,7 +55,24 @@ CREATE TABLE auditoria.log_persona_fisica(
     valor_previo varchar(255),
     valor_posterior varchar(255)
 )
-----FUNCIONES ------------
+
+CREATE TABLE auditoria.log_persona_juridica(
+    identificador integer not null,
+    usuario varchar(255) not null,
+    fecha date not null,
+    tipo_instruccion varchar(60) not null,
+    valor_previo varchar(255),
+    valor_posterior varchar(255)
+)
+CREATE TABLE auditoria.log_organismo_entidad(
+    identificador integer not null,
+    usuario varchar(255) not null,
+    fecha date not null,
+    tipo_instruccion varchar(60) not null,
+    valor_previo varchar(255),
+    valor_posterior varchar(255)
+)
+----FUNCIONES  FISICA------------
 CREATE OR REPLACE FUNCTION tr_persona_fisica_insert()
 RETURNS TRIGGER
 LANGUAGE plpgsql
@@ -63,7 +80,102 @@ AS
 $$
 DECLARE
 BEGIN
-    INSERT INTO auditoria.log_persona_fisica VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'INSERT',null,NEW);
+    INSERT INTO auditoria.log_persona_fisica VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'INSERT',NULL,NEW);
+	RETURN null;
+END;
+$$
+
+CREATE OR REPLACE FUNCTION tr_persona_fisica_update()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_persona_fisica VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'UPDATE',OLD,NEW);
+	RETURN null;
+END;
+$$
+CREATE OR REPLACE FUNCTION tr_persona_fisica_delete()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_persona_fisica VALUES (OLD.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'DELETE',OLD,NULL);
+	RETURN null;
+END;
+$$
+
+----FUNCIONES  JURIDICA------------
+CREATE OR REPLACE FUNCTION tr_persona_juridica_insert()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_persona_juridica VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'INSERT',NULL,NEW);
+	RETURN null;
+END;
+$$
+
+CREATE OR REPLACE FUNCTION tr_persona_juridica_update()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_persona_juridica VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'UPDATE',OLD,NEW);
+	RETURN null;
+END;
+$$
+CREATE OR REPLACE FUNCTION tr_persona_juridica_delete()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_persona_juridica VALUES (OLD.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'DELETE',OLD,NULL);
+	RETURN null;
+END;
+$$
+
+----FUNCIONES  ORGANISMO ENTIDAD------------
+CREATE OR REPLACE FUNCTION tr_organismo_entidad_insert()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_organismo_entidad VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'INSERT',NULL,NEW);
+	RETURN null;
+END;
+$$
+
+CREATE OR REPLACE FUNCTION tr_organismo_entidad_update()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_organismo_entidad VALUES (NEW.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'UPDATE',OLD,NEW);
+	RETURN null;
+END;
+$$
+CREATE OR REPLACE FUNCTION tr_organismo_entidad_delete()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+BEGIN
+    INSERT INTO auditoria.log_organismo_entidad VALUES (OLD.id,SESSION_USER,(SELECT CURRENT_TIMESTAMP),'DELETE',OLD,NULL);
 	RETURN null;
 END;
 $$
